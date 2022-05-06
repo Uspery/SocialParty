db6e41076a168535b233c70ace786ac04123252d
Kaua Vitorio <kauavitorioof@gmail.com>
2022-05-06 08:18:04 -0300

    Create first Activities

---
+++ app/src/main/java/dev/uspery/socialparty/Activities/Auth/SignInActivity.java
@@ -0,0 +1,16 @@
+package dev.uspery.socialparty.Activities.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.uspery.socialparty.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
