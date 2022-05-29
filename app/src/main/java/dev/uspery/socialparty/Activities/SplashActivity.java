331f20f9a33b306b4d0e45b0bd23010df30bc796
Kauavitorio <kauavitorioof@gmail.com>
2022-05-29 09:14:51 -0300

    Re order base files for development

--- app/src/main/java/dev/uspery/socialparty/Activities/SplashActivity.java
+++ app/src/main/java/dev/uspery/socialparty/Activities/SplashActivity.java
@@ -2,6 +2,7 @@ package dev.uspery.socialparty.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import dev.uspery.socialparty.R;
@@ -12,5 +13,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // While the app is under development you can choose what
+        //  Screen will open here, all that you need to do is change
+        //  the variable intent
+        Class<?> intent = SplashActivity.class;
+        if(intent != SplashActivity.class)
+            startActivity(new Intent(this, intent));
     }
 }
